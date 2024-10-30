2b46c0968cc923172ccf7d41dacab67e1ac70d74
Michel Pan <michelpan+code@hotmail.com>
2024-10-30 23:33:47 +0100

    feat: add minimal metrics

--- app.py
+++ app.py
@@ -1,15 +1,68 @@
-from utils.belasting import belastingstelsel
 import streamlit as st
 
+from utils.Belasting import Belasting
+from utils.belastingstelsel import belastingstelsels
+from utils.Salaris import Salaris
 
+# Global predefined settings
+input_belastingjaar = 2024
+input_maand_of_jaar = "Maandelijks"
 
 with st.sidebar:
-    st.write("Vul hier je gegevens in")
-    input_salaris = st.number_input(label="Bruto Salaris (€/maand)", value=3500.00, min_value=0.00)
-    input_vakantiegeld = st.number_input(label="Vakantiegeld (%)", value=8.00, min_value=8.00)
-    input_13e = st.number_input(label="13e maand (%)", value=100/12, min_value=0.0)
-    input_bonus_abs = st.number_input(label="Bonus (€)", value=0.00, min_value=0.00)
-    input_bonus_perc = st.number_input(label="Bonus (%)", value=0.00, min_value=0.00)
 
-st.metric(label="Netto Per Maand", value=f"€{input_salaris}")
-st.metric(label="Stijging tot volgende 100 euro", value=f"Bruto - x")
+    with st.popover("Opties"):
+        input_belastingjaar: int = st.selectbox(
+            label="Belastingjaar", options=[2024, 2025]
+        )
+        input_maand_of_jaar: str = st.selectbox(
+            label="Salarisbasis", options=["Maandelijks", "Jaarlijks"]
+        )
+
+        belasting = Belasting(jaar=input_belastingjaar)
+
+    if input_maand_of_jaar == "Maandelijks":
+        st.write(f"*Belastingjaar **{input_belastingjaar}***")
+
+        input_salaris: float = st.number_input(
+            label="Bruto Salaris (€/maand)", value=3500.00, min_value=0.00
+        )
+        input_vakantiegeld: float = st.number_input(
+            label="Vakantiegeld (%)", value=8.00, min_value=8.00
+        )
+        input_eindejaars: float = st.number_input(
+            label="Eindejaarsuitkering (%)", value=100 / 12, min_value=0.0
+        )
+        input_bonus_abs: float = st.number_input(
+            label="Bonus (€)", value=0.00, min_value=0.00
+        )
+        input_bonus_perc: float = st.number_input(
+            label="Bonus (%)", value=0.00, min_value=0.00
+        )
+    elif input_maand_of_jaar == "Jaarlijks":
+        input_salaris_jaar = st.number_input(
+            label="Bruto Salaris (€/jaar)", value=45000.00, min_value=0.00
+        )
+
+
+salaris = Salaris(
+    bruto_per_maand=input_salaris,
+    percentage_vakantiegeld=input_vakantiegeld,
+    percentage_eindejaars=input_eindejaars,
+    percentage_bonus=input_bonus_perc,
+    bonus=input_bonus_abs,
+)
+salaris_bruto_jaar = salaris.bereken_bruto_jaarlijks()
+salaris_netto_jaar = salaris.bereken_netto_jaarlijks(belasting=belasting)
+salaris_netto_maand = salaris_netto_jaar / 12
+st.metric(
+    label="Bruto (Jaar)",
+    value=f"€{salaris_bruto_jaar:,.2f}",
+)
+st.metric(
+    label="Netto (Jaar)",
+    value=f"€{salaris_netto_jaar:,.2f}",
+)
+st.metric(
+    label="Netto (Maand)",
+    value=f"€{salaris_netto_maand:,.2f}",
+)
