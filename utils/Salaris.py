f35763239b694432c5cd02086e07f604b2c4ae61
Li Yong Pan <michelpan+code@hotmail.com>
2024-11-01 19:24:32 +0100

    feat: add bruto-netto uitruil

--- utils/Salaris.py
+++ utils/Salaris.py
@@ -10,6 +10,7 @@ class Salaris:
         percentage_bonus: float = 0,
         percentage_pensioen: float = 0,
         bonus: float = 0,
+        bruto_netto_ruil: float = 0,
     ):
         self.bruto_per_maand = bruto_per_maand
         self.percentage_vakantiegeld = percentage_vakantiegeld
@@ -17,6 +18,7 @@ class Salaris:
         self.percentage_bonus = percentage_bonus
         self.percentage_pensioen = percentage_pensioen
         self.bonus = bonus
+        self.bruto_netto_ruil = bruto_netto_ruil
 
     def bereken_bruto_jaarlijks(self) -> float:
         som_percentages = (
@@ -26,8 +28,11 @@ class Salaris:
             - self.percentage_pensioen
         )
         percentage = (100 + som_percentages) / 100
-        return (12 * self.bruto_per_maand * percentage) + self.bonus
+
+        maandelijks_salaris = self.bruto_per_maand - self.bruto_netto_ruil
+        return (12 * maandelijks_salaris * percentage) + self.bonus
 
     def bereken_netto_jaarlijks(self, belasting: "Belasting") -> float:
         bruto_jaarlijks = self.bereken_bruto_jaarlijks()
-        return belasting.bereken_netto_salaris(bruto_jaarlijks)
+        netto_jaarlijks = belasting.bereken_netto_salaris(bruto_jaarlijks)
+        return netto_jaarlijks + 12 * self.bruto_netto_ruil
